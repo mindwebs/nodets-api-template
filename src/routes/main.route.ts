bd2ec0667f757bf0962a1121b3224f36a2acf205
singhayushh <coding.ayush@gmail.com>
2023-03-21 19:21:04 +0530

    style: major update in eslint config

--- src/routes/main.route.ts
+++ src/routes/main.route.ts
@@ -6,7 +6,7 @@ const mainRouter: Router = Router();
 // Add routes defined in other files below.
 
 mainRouter.use((req: Request, res: Response) => {
-    GenerateResponse(res, 404)
+    GenerateResponse(res, 404);
 });
 
 export { mainRouter };
